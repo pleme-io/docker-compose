70bc65bb82bd00fe0f069dbb56f8d9672d6ecaae
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:59 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Akeyless Docker Compose quickstart for gateway, SRA, cache, and monitoring";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ docker-compose ];
+      };
+    });
+}
