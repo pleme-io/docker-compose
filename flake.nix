0ee570f2be45a8ac9a67b78f9b3d3687372b8a3c
drzln <luis.d@akeyless.io>
2026-06-08 17:41:41 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "Akeyless Docker Compose quickstart for gateway, SRA, cache, and monitoring";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
